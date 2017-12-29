240870ed332cbff6a995d022f86554bbe9f7bb67
Aurélien Bottazzini <aurelien@bottazini.com>
2017-12-29 11:54:39 +0100

    training update

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[cider/cider-nrepl "0.15.1-SNAPSHOT"]])
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
+                 [zjhmale/letrec "0.1.0"]]
+  :plugins [[cider/cider-nrepl "0.16.0-SNAPSHOT"]])
